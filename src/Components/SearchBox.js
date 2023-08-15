b58958ac2ce1f76c533c02b31050b7ce6de4df8c
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-08-15 12:22:13 +0530

    Basic setup
    - Removed unnecessary files
    - added a search box
    - added a placeholder background image

---
+++ src/Components/SearchBox.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function SearchBox() {
+  return (
+    <div className="searchBox">
+      <div className="question">What did Seneca say about </div>
+      <div>
+        <input type="text" />
+        <span> ?</span>
+      </div>
+    </div>
+  );
+}
